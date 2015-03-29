6fc23641751047664096a6237d049f7877e59983
francoischalifour <francois.chalifour@gmail.com>
2015-03-29 17:05:25 +0200

    Ajout de documentations

--- app/modules/vote/vote.controller.js
+++ app/modules/vote/vote.controller.js
@@ -4,7 +4,19 @@ var Vote = require('./vote.model');
 
 var path = './vote/views/';
 
+/**
+ * Deletes a vote.
+ * @param {object}   req
+ * @param {object}   res
+ * @param {function} next
+ */
 module.exports.Delete = function(req, res, next) {
+    // If the user is not logged in.
+    if (!req.session.userid || !req.session.username) {
+        res.redirect('/login');
+        return;
+    }
+
     Vote.deleteVoteById(req.params.id, req.session.userid, function(err, result) {
         if (err) {
             console.log(err);
